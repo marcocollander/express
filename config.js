9a262553a241eab0c6f8ea512504fea57d8e117d
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-11-27 14:52:42 +0100

    Add admin login, admin news

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: 'mongodb+srv://zenonadmin:UP129HooHA9HaIjW@cluster0.imnqcq9.mongodb.net/?retryWrites=true&w=majority',
+  db: '',
   keySesion: ['Your kye 1956'],
   maxAgeSesion: 24 * 60 * 60 * 1000,
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const News = require('../models/news');
+
 const router = express.Router();
 
 router.all('*', (req, res, next) => {
   if (!req.session.admin) {
     res.redirect('login');
+
     return;
   }
 
@@ -13,24 +15,35 @@ router.all('*', (req, res, next) => {
 
 /* GET quiz page. */
 router.get('/', (req, res) => {
-  res.render('admin/index', { title: 'Admin' });
+  News.find({}, (err, data) => {
+    res.render('admin/index', { title: 'Admin', data });
+  });
 });
 
 router.get('/news/add', (req, res) => {
-  res.render('admin/news-form', { title: 'Dodaj news', errors: {} });
+  res.render('admin/news-form', { title: 'Dodaj news', errors: {}, body: {} });
 });
 
 router.post('/news/add', (req, res) => {
   const body = req.body;
 
   const newsData = new News(body);
-  const errors = newsData.validateSync()
+  const errors = newsData.validateSync();
 
   newsData.save((err) => {
-    console.log(err);
-  });
+    if (err) {
+      res.render('admin/news-form', { title: 'Dodaj news', errors, body });
+      return;
+    }
 
-  res.render('admin/news-form', { title: 'Dodaj news', errors });
+    res.redirect('/admin');
+  });
+});
+
+router.get('/news/delete/:id', (req, res) => {
+  News.findByIdAndDelete(req.params.id, (err) => {
+    res.redirect('/admin');
+  });
 });
 
 module.exports = router;
