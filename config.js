47b1277289e67c3083a82212fabb8977f3375fe0
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-11-29 21:56:41 +0100

    Add quiz form

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: '',
+  db: 'mongodb+srv://zenonadmin:UP129HooHA9HaIjW@cluster0.imnqcq9.mongodb.net/?retryWrites=true&w=majority',
   keySesion: ['Your kye 1956'],
   maxAgeSesion: 24 * 60 * 60 * 1000,
 };
--- routes/quiz.js
+++ routes/quiz.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/qiuz');
 
 /* GET quiz page. */
 router.get('/', (req, res) => {
-  const url = req.url;
-  res.render('quiz', { title: 'Quiz' });
+  const show = !req.session.vote;
+  Quiz.find({}, (err, data) => {
+    let sum = 0;
+    data.forEach((item) => {
+      sum += item.vote;
+    });
+
+    res.render('quiz', { title: 'Quiz', data, show, sum });
+  });
+
+  // new Quiz({
+  //   title: 'Ile koncertów fortepianowych skompponował Chopin',
+  //   vote: 0,
+  // }).save(); Tworzy model w bazie mongoDB
+});
+
+router.post('/', (req, res) => {
+  const id = req.body.quiz;
+
+  Quiz.findOne({ _id: id }, (err, data) => {
+    data.vote = ++data.vote;
+    data.save((err) => {
+      req.session.vote = 1;
+      res.redirect('/quiz');
+    });
+  });
 });
 
 module.exports = router;
