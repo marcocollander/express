e208d9dbf88a246049fb5ce5c8509719d7ab27d2
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-11-29 07:58:35 +0100

    Add news filter

--- routes/news.js
+++ routes/news.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news');
 
 /* GET news page. */
 router.get('/', (req, res) => {
-  const url = req.url;
-  res.render('news', { title: 'News' });
+  const search = req.query.search || '';
+  const findNews = News.find({ title: new RegExp(search.trim(), 'i') }).sort({
+    created: -1,
+  });
+
+  findNews.exec((err, data) => {
+    res.render('news', { title: 'News', data, search });
+  });
 });
 
 module.exports = router;
